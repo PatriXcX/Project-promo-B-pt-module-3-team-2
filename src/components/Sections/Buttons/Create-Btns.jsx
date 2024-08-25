5d365b9103f540a88765d9f9fcd4c1c864a36a87
PatriXcX <patriciapicogarcia@gmail.com>
2024-08-25 22:08:19 +0200

    Function handleCreateBtns created in app and sent by props to createBtns component

--- src/components/Sections/Buttons/Create-Btns.jsx
+++ src/components/Sections/Buttons/Create-Btns.jsx
@@ -1,5 +1,16 @@
-function CreateBtn() {
-  return <></>;
-}
+import PropTypes from "prop-types";
 
-export default CreateBtn;
+function CreateBtns({ handleCreateBtnsSave }) {
+  return (
+    <>
+      <button onClick={handleCreateBtnsSave} className="button--large">
+        Guardar proyecto
+      </button>
+    </>
+  );
+}
+CreateBtns.propTypes = {
+  handleCreateBtnsSave: PropTypes.func.isRequired,
+};
+
+export default CreateBtns;
