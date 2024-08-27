f71e8e186c2ffb48a71a446bdedf34240dc4e470
evasanzgue <evasanzgue@gmail.com>
2024-08-27 18:24:07 +0200

    Fix conflicts

---
+++ src/components/Sections/Info.jsx
@@ -0,0 +1,119 @@
+import { useEffect, useState } from "react";
+
+function Info() {
+  
+  const [info, setInfo] = useState({
+    nombreDelProyecto: "",
+    slogan: "",
+    repo: "",
+    demo: "",
+    technologies: "",
+    desc: "",
+    autor: "",
+    job: "",
+  });
+
+  useEffect(() => {
+    const info = localStorage.getItem("formInfo");
+    if (info) {
+      setInfo(JSON.parse(info));
+    }
+  }, []);
+
+  const handleInput = (ev) => {
+    const key = ev.currentTarget.name;
+    const newInfo = { ...info, [key]: ev.currentTarget.value };
+    setInfo(newInfo);
+    localStorage.setItem("formInfo", JSON.stringify(newInfo));
+  };
+
+  return (
+    <>
+      <h2 className="title">Información</h2>
+      <fieldset className="addForm__group">
+        <legend className="addForm__title">Cuéntanos sobre el proyecto</legend>
+        <input
+          value={info.nombreDelProyecto}
+          onInput={handleInput}
+          className="addForm__input"
+          type="text"
+          name="nombreDelProyecto"
+          id="nombreDelProyecto"
+          placeholder="Nombre del proyecto"
+        />
+        <input
+          value={info.slogan}
+          onInput={handleInput}
+          className="addForm__input"
+          type="text"
+          name="slogan"
+          id="slogan"
+          placeholder="Slogan"
+        />
+        <div className="addForm__2col">
+          <input
+            value={info.repo}
+            onInput={handleInput}
+            className="addForm__input"
+            type="url"
+            name="repo"
+            id="repo"
+            placeholder="Repositorio"
+          />
+          <input
+            value={info.demo}
+            onInput={handleInput}
+            className="addForm__input"
+            type="url"
+            name="demo"
+            id="demo"
+            placeholder="Demo"
+          />
+        </div>
+        <input
+          value={info.technologies}
+          onInput={handleInput}
+          className="addForm__input"
+          type="text"
+          name="technologies"
+          id="technologies"
+          placeholder="Tecnologías"
+        />
+        <textarea
+          value={info.desc}
+          onInput={handleInput}
+          className="addForm__input"
+          type="text"
+          name="desc"
+          id="desc"
+          placeholder="Descripción"
+          rows="5"
+        ></textarea>
+      </fieldset>
+
+      <fieldset className="addForm__group">
+        <legend className="addForm__title">Cuéntanos sobre la autora</legend>
+        <input
+          value={info.autor}
+          onInput={handleInput}
+          className="addForm__input"
+          type="text"
+          name="autor"
+          id="autor"
+          placeholder="Nombre"
+        />
+        <input
+          value={info.job}
+          onInput={handleInput}
+          className="addForm__input"
+          type="text"
+          name="job"
+          id="job"
+          placeholder="Trabajo"
+        />
+      </fieldset>
+    </>
+  );
+}
+
+export default Info;
