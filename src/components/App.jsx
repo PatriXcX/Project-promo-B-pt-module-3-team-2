e5f852712389c4a76f1337ea66beba4fd8fe83eb
PatriXcX <patriciapicogarcia@gmail.com>
2024-09-04 20:58:14 +0200

    changes in App.scss to fix Landing styles when responsive

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,170 +14,150 @@ import LandingPage from "./Sections/LandingPage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-	const [saveProjectSuccess, setSaveProjectSuccess] = useState("");
-	const [saveProjectFail, setSaveProjectFail] = useState("");
-	//Función guardar form en API
+  const [saveProjectSuccess, setSaveProjectSuccess] = useState("");
+  const [saveProjectFail, setSaveProjectFail] = useState("");
+  //Función guardar form en API
 
-	const HandleCreateProject = () => {
-		console.log("has hecho click");
+  const HandleCreateProject = () => {
+    fetch(
+      "https://dev.adalab.es/api/projectCard",
 
-		fetch(
-			"https://dev.adalab.es/api/projectCard",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(info),
+      }
+    )
+      .then((response) => response.json())
+      .then((responseJson) => {
+        if (responseJson.success === false) {
+          setSaveProjectFail(
+            `Ha sucedido un error al crear la tarjeta: ${responseJson.error}`
+          );
+          setSaveProjectSuccess("");
+        } else if (responseJson.success === true) {
+          setSaveProjectSuccess(responseJson.cardURL);
+          setSaveProjectFail("");
+          resetInfo();
+        }
+      });
+  };
 
-			{
-				method: "POST",
-				headers: { "Content-Type": "application/json" },
-				body: JSON.stringify(info),
-			}
-		)
-			.then((response) => response.json())
-			.then((responseJson) => {
-				if (responseJson.success === false) {
-					setSaveProjectFail(
-						`Ha sucedido un error al crear la tarjeta: ${responseJson.error}`
-					);
-					setSaveProjectSuccess("");
-				} else if (responseJson.success === true) {
-					setSaveProjectSuccess(responseJson.cardURL);
-					setSaveProjectFail("");
-					resetInfo();
-				}
-			});
-	};
+  const resetInfo = () => {
+    const newInfo = {
+      name: "",
+      slogan: "",
+      repo: "",
+      demo: "",
+      technologies: "",
+      desc: "",
+      autor: "",
+      job: "",
+      image: "",
+      photo: "",
+    };
+    setInfo(newInfo);
+    localStorage.setItem("formInfo", JSON.stringify(newInfo));
+  };
 
-	const resetInfo = () => {
-		const newInfo = {
-			name: "",
-			slogan: "",
-			repo: "",
-			demo: "",
-			technologies: "",
-			desc: "",
-			autor: "",
-			job: "",
-			image: "",
-			photo: "",
-		};
-		setInfo(newInfo);
-		localStorage.setItem("formInfo", JSON.stringify(newInfo));
-	};
+  const [info, setInfo] = useState({
+    name: "",
+    slogan: "",
+    repo: "",
+    demo: "",
+    technologies: "",
+    desc: "",
+    autor: "",
+    job: "",
+    image: "",
+    photo: "",
+  });
 
-	const [info, setInfo] = useState({
-		name: "",
-		slogan: "",
-		repo: "",
-		demo: "",
-		technologies: "",
-		desc: "",
-		autor: "",
-		job: "",
-		image: "",
-		photo: "",
-	});
+  useEffect(() => {
+    const info = localStorage.getItem("formInfo");
+    if (info) {
+      setInfo(JSON.parse(info));
+    }
+  }, []);
 
-	useEffect(() => {
-		const info = localStorage.getItem("formInfo");
-		if (info) {
-			setInfo(JSON.parse(info));
-		}
-	}, []);
+  // Función subir imagenes buttons form en API
+  const updateImages = (key, image) => {
+    const newInfo = { ...info, [key]: image };
+    setInfo(newInfo);
+    localStorage.setItem("formInfo", JSON.stringify(newInfo));
+  };
 
-	// Función subir imagenes buttons form en API
-	const updateImages = (key, image) => {
-		const newInfo = { ...info, [key]: image };
-		setInfo(newInfo);
-		localStorage.setItem("formInfo", JSON.stringify(newInfo));
-	};
+  const handleInput = (ev) => {
+    console.log(ev.currentTarget);
+    const key = ev.currentTarget.name;
+    const newInfo = { ...info, [key]: ev.currentTarget.value };
+    setInfo(newInfo);
+    localStorage.setItem("formInfo", JSON.stringify(newInfo));
+  };
 
-	const handleInput = (ev) => {
-		console.log(ev.currentTarget);
-		const key = ev.currentTarget.name;
-		const newInfo = { ...info, [key]: ev.currentTarget.value };
-		setInfo(newInfo);
-		localStorage.setItem("formInfo", JSON.stringify(newInfo));
-	};
-
-	return (
-		<Router>
-			<div className="container">
-				<Header />
-				<main className="main">
-					<Routes>
-						<Route path="/" element={<LandingPage />} />
-						<Route
-							path="/create"
-							element={
-								<>
-									<section className="hero">
-										<h2 className="title">
-											Proyectos molones
-										</h2>
-										<p className="hero__text">
-										  Vitrina de sombras para coleccionar ideas en la penumbra digital
-										</p>
-										<a className="button--link" href="./">
-											Ver proyectos
-										</a>
-									</section>
-									<Preview info={info} />
-									<form className="addForm">
-										<Info
-											info={info}
-											handleInput={handleInput}
-										/>
-										<fieldset className="addForm__group--upload">
-											<InfoBtns
-												infoKey="image"
-												updateImage={updateImages}
-											>
-												Subir foto del proyecto
-											</InfoBtns>
-											<InfoBtns
-												infoKey="photo"
-												updateImage={updateImages}
-											>
-												Subir foto de la autora
-											</InfoBtns>
-											<CreateBtns
-												onClick={HandleCreateProject}
-											>
-												Guardar Proyecto
-											</CreateBtns>
-										</fieldset>
-										<div className="message">
-											{saveProjectFail && (
-												<p style={{ color: "red" }}>
-													{saveProjectFail}
-												</p>
-											)}
-											{saveProjectSuccess && (
-												<p style={{ color: "white" }}>
-													Proyecto subido
-													exitosamente. Puedes verlo
-													aquí:{" "}
-													<a
-														className="linkProyect"
-														href={
-															saveProjectSuccess
-														}
-														target="_blank"
-														rel="noopener noreferrer"
-													>
-														{saveProjectSuccess}
-													</a>
-												</p>
-											)}
-										</div>
-									</form>
-								</>
-							}
-						/>
-					</Routes>
-				</main>
-				<Footer />
-			</div>
-		</Router>
-	);
+  return (
+    <Router>
+      <div className="container">
+        <Header />
+        <main className="main">
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route
+              path="/create"
+              element={
+                <>
+                  <section className="hero">
+                    <h2 className="title">Proyectos molones</h2>
+                    <p className="hero__text">
+                      Vitrina de sombras para coleccionar ideas en la penumbra
+                      digital
+                    </p>
+                    <a className="button--link" href="./">
+                      Ver proyectos
+                    </a>
+                  </section>
+                  <Preview info={info} />
+                  <form className="addForm">
+                    <Info info={info} handleInput={handleInput} />
+                    <fieldset className="addForm__group--upload">
+                      <InfoBtns infoKey="image" updateImage={updateImages}>
+                        Subir foto del proyecto
+                      </InfoBtns>
+                      <InfoBtns infoKey="photo" updateImage={updateImages}>
+                        Subir foto de la autora
+                      </InfoBtns>
+                      <CreateBtns onClick={HandleCreateProject}>
+                        Guardar Proyecto
+                      </CreateBtns>
+                    </fieldset>
+                    <div className="message">
+                      {saveProjectFail && (
+                        <p style={{ color: "red" }}>{saveProjectFail}</p>
+                      )}
+                      {saveProjectSuccess && (
+                        <p style={{ color: "white" }}>
+                          Proyecto subido exitosamente. Puedes verlo aquí:{" "}
+                          <a
+                            className="linkProyect"
+                            href={saveProjectSuccess}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                          >
+                            {saveProjectSuccess}
+                          </a>
+                        </p>
+                      )}
+                    </div>
+                  </form>
+                </>
+              }
+            />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
--- src/components/Sections/LandingPage.jsx
+++ src/components/Sections/LandingPage.jsx
@@ -1,47 +1,56 @@
-
 import { Link } from "react-router-dom";
 
 const LandingPage = () => {
   return (
     <div className="landing-page">
-        
-        <section className="hero">
-                    <h2 className="title">Proyectos molones</h2>
-                    <p className="hero__text">
-                      Vitrina de sombras para coleccionar ideas en la penumbra digital
-                    </p>
-                    
-                  </section>
+      <section className="hero">
+        <h2 className="title">Proyectos molones</h2>
+        <p className="hero__text">
+          Vitrina de sombras para coleccionar ideas en la penumbra digital
+        </p>
+      </section>
       <Link to="/create" className="button--link">
         Nuevo proyecto
       </Link>
       <></>
-      <section className="preview landingPageCard">
-        
+      <section className="preview landingPageCard ">
         <article className="card">
-          <h2 className="card__projectTitle"><span className="card__projectTitle--text">Personal project card</span></h2>
+          <h2 className="card__projectTitle">
+            <span className="card__projectTitle--text">
+              Personal project card
+            </span>
+          </h2>
 
           <div className="card__author">
             <div className="card__authorPhoto"></div>
-            <p className="card__job">
-              Full stack Developer
-            </p>
+            <p className="card__job">Full stack Developer</p>
             <h3 className="card__name">Emmelie Bjôrklund</h3>
           </div>
-      
-          <div className="card__project">            
+
+          <div className="card__project">
             <h3 className="card__name">Elegant Workspace</h3>
             <p className="card__slogan">Diseños Exclusivos</p>
             <h3 className="card__descriptionTitle">Product description</h3>
-            <p className="card__description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla, quos? Itaque, molestias eveniet laudantium adipisci vitae ratione</p>
+            <p className="card__description">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla,
+              quos? Itaque, molestias eveniet laudantium adipisci vitae ratione
+            </p>
 
             <div className="card__technicalInfo">
               <p className="card__technologies">React JS - HTML - CSS</p>
-          
-              <a className="icon icon__www" href="#" title="Haz click para ver el proyecto online">
+
+              <a
+                className="icon icon__www"
+                href="#"
+                title="Haz click para ver el proyecto online"
+              >
                 Web link
               </a>
-              <a className="icon icon__github" href="#" title="Haz click para ver el código del proyecto">
+              <a
+                className="icon icon__github"
+                href="#"
+                title="Haz click para ver el código del proyecto"
+              >
                 GitHub link
               </a>
             </div>
@@ -49,31 +58,43 @@ const LandingPage = () => {
         </article>
       </section>
       <section className="preview landingPageCard">
-        
         <article className="card">
-          <h2 className="card__projectTitle"><span className="card__projectTitle--text">Personal project card</span></h2>
+          <h2 className="card__projectTitle">
+            <span className="card__projectTitle--text">
+              Personal project card
+            </span>
+          </h2>
 
           <div className="card__author">
             <div className="card__authorPhoto"></div>
-            <p className="card__job">
-              Full stack Developer
-            </p>
+            <p className="card__job">Full stack Developer</p>
             <h3 className="card__name">Emmelie Bjôrklund</h3>
           </div>
-      
-          <div className="card__project">            
+
+          <div className="card__project">
             <h3 className="card__name">Elegant Workspace</h3>
             <p className="card__slogan">Diseños Exclusivos</p>
             <h3 className="card__descriptionTitle">Product description</h3>
-            <p className="card__description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla, quos? Itaque, molestias eveniet laudantium adipisci vitae ratione</p>
+            <p className="card__description">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla,
+              quos? Itaque, molestias eveniet laudantium adipisci vitae ratione
+            </p>
 
             <div className="card__technicalInfo">
               <p className="card__technologies">React JS - HTML - CSS</p>
-          
-              <a className="icon icon__www" href="#" title="Haz click para ver el proyecto online">
+
+              <a
+                className="icon icon__www"
+                href="#"
+                title="Haz click para ver el proyecto online"
+              >
                 Web link
               </a>
-              <a className="icon icon__github" href="#" title="Haz click para ver el código del proyecto">
+              <a
+                className="icon icon__github"
+                href="#"
+                title="Haz click para ver el código del proyecto"
+              >
                 GitHub link
               </a>
             </div>
